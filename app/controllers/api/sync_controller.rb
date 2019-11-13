39403d34c2398b86b247af629d0714d6655ec5b0
Gleidson Semchechen <gleids@gmail.com>
2019-11-13 01:08:58 -0300

    Ajuste das rotas e criação do metodo Post

--- app/controllers/api/sync_controller.rb
+++ app/controllers/api/sync_controller.rb
@@ -1,19 +1,47 @@
 # app/controllers/api/sync_controller.rb
 class Api::SyncController < Api::BaseController
+    
+    
     def index
-        #o = fakeobj
-        o = fakeobj2
-        #render json: o
-        #render :json => o, :include => :posts 
+        # Teste inicial para montagem do objeto e saída da API
+        t = testeObject
+        render :json => t, :include => [:payments, :buyer, :shipping => { :include => :receiver_address  }, :order_items => { :include => :item}] 
 
-        render :json => o, :include => [:payments, :order_items => { :include => :item}] 
 
 
     end
 
-    def fakeobj2
+    # POST /api/sync
 
-        params = {
+    def create
+ 
+        #@contact = Contact.new(contact_params.merge(user: current_user))
+        
+        sync_params
+        @sync = Sync.new()
+      
+        if @sync.save
+      
+          render json: "Salvou", status: :created
+      
+        else
+      
+          render json: @sync.errors, status: :unprocessable_entity
+      
+        end
+      
+    end
+
+    def sync_params
+ 
+        params.require(:id)
+        #params.require(:contact).permit(:name, :email, :phone, :description)
+    
+      end
+
+    def testeObject
+
+        par = {
             external_id: 9987071,
             store_id: 282,
             date_created: "2019-06-24T16:45:32.000-04:00",
@@ -53,14 +81,57 @@ class Api::SyncController < Api::BaseController
                     date_approved: "2019-06-24T16:45:35.000-04:00",
                     date_created: "2019-06-24T16:45:33.000-04:00"
                 }
-            ]
-          }
+            ],
+            shipping_attributes: {
+                external_id: 43444211797,
+                shipment_type: "shipping",
+                date_created: "2019-06-24T16:45:33.000-04:00",
+                receiver_address_attributes: {
+                    external_id: 1051695306,
+                    address_line: "Rua Fake de Testes 3454",
+                    street_name: "Rua Fake de Testes",
+                    street_number: "3454",
+                    comment: "teste",
+                    zip_code: "85045020",
+                    city_attributes: {
+                        name: "Cidade de Testes"
+                    },
+                    state_attributes: {
+                        "name": "São Paulo"
+                    },
+                    country_attributes: {
+                        external_id: "BR",
+                        name: "Brasil"
+                    },
+                    neighborhood_attributes: {
+                        name: "Vila de Testes"
+                    },
+                    latitude: -23.629037,
+                    longitude: -46.712689,
+                    receiver_phone: "41999999999"
+                }
+            },
+            buyer_attributes: {
+                external_id: 136226073,
+                nickname: "JOHN DOE",
+                email: "john@doe.com",
+                phone_attributes: {
+                   area_code: 41,
+                   number: "999999999"
+                },
+                first_name: "John",
+                last_name: "Doe",
+                billing_info_attributes: {
+                   doc_type: "CPF",
+                   doc_number: "09487965477"
+                }
+             }
 
-        o  = Order.create(params)
+        }
+        o  = Order.create(par)
                 
-
+        o.save
             
-        
         return o 
     end
 
